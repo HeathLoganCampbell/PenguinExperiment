46bdac8abff01a63b6137ad2669fa55f17dace2e
HeathLoganCampbell <Heath.campbell2@datacom.co.nz>
2025-03-06 02:34:16 +1100

    add walk

--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -25,20 +25,8 @@ export default class Level extends Phaser.Scene {
 		welcome.text = "Phaser 3 + Phaser Editor v4";
 		welcome.setStyle({ "fontFamily": "Arial", "fontSize": "30px" });
 
-		let mouseX = this.input.activePointer.x;
-		let mouseY = this.input.activePointer.y;
-
-		let dx = (mouseX) - 626
-        let dy = (mouseY  + 80) - 310
-        let rawAngle = Math.floor((Math.atan2(dy, dx) * (180 / Math.PI)) - 90)
-        var angle = (rawAngle < 0) ? rawAngle + 360 : rawAngle
-		let direction = Math.round(angle / 45) + 1
-
-		var directionId = (direction > 8) ? 1 : direction;
-		var animId = 1;
-
 		// body
-		const body = this.add.sprite(626, 310, "penguin_1", "body/" + directionId + "_" + animId);
+		const body = this.add.sprite(626, 310, "penguin_1", "body/1_1");
 		body.scaleX = 2;
 		body.scaleY = 2;
 		body.tintTopLeft = 6684825;
@@ -46,9 +34,8 @@ export default class Level extends Phaser.Scene {
 		body.tintBottomLeft = 6684825;
 		body.tintBottomRight = 6684825;
 
-
 		// penguin
-		const penguin = this.add.sprite(626, 310, "penguin_1", "penguin/" + directionId + "_" + animId);
+		const penguin = this.add.sprite(626, 310, "penguin_1", "penguin/1_1");
 		penguin.scaleX = 2;
 		penguin.scaleY = 2;
 
@@ -74,28 +61,48 @@ export default class Level extends Phaser.Scene {
 
 		this.editorCreate();
 
-		// this.dino.on("pointerover", () => {
-
-		// 	this.welcome.text = "aaa!";
-		// });
-
 		this.penguin.on("pointerover", () => {
 			this.welcome.text = "Penguin!";
+			this.sit = true;
 		});
 
+		this.count = 0; // Initialize count
+
+		this.time.addEvent({
+			delay: 100,
+			callback: () => {
+				this.count++;
+
+				let dx = this.lastMouseX - 626;
+				let dy = (this.lastMouseY + 80) - 310;
+				let rawAngle = Math.floor((Math.atan2(dy, dx) * (180 / Math.PI)) - 90);
+				let angle = (rawAngle < 0) ? rawAngle + 360 : rawAngle;
+				var directionId;
+				let walkFrame = 1;
+				if (this.sit)
+				{
+					let direction = Math.round(angle / 45) + 1;
+					directionId = ((direction > 8) ? 1 : direction) + 16;
+					walkFrame = 1;
+				}
+				else 
+				{
+					let direction = Math.round(angle / 90) + 1;
+					directionId = ((direction > 8) ? 1 : direction) + 10;
+					walkFrame = (this.count % 8) + 1;
+				}
+
+				this.body.setTexture("penguin_1", `body/${directionId}_${walkFrame}`);
+				this.penguin.setTexture("penguin_1", `penguin/${directionId}_${walkFrame}`);
+			},
+			loop: true 
+		});
+
+
+
 		this.input.on('pointermove', (pointer) => {
-			let mouseX = pointer.x;
-			let mouseY = pointer.y;
-
-			let dx = mouseX - 626;
-			let dy = (mouseY + 80) - 310;
-			let rawAngle = Math.floor((Math.atan2(dy, dx) * (180 / Math.PI)) - 90);
-			let angle = (rawAngle < 0) ? rawAngle + 360 : rawAngle;
-			let direction = Math.round(angle / 45) + 1;
-			let directionId = (direction > 8) ? 1 : direction;
-
-			this.body.setTexture("penguin_1", `body/${directionId}_1`);
-			this.penguin.setTexture("penguin_1", `penguin/${directionId}_1`);
+			this.lastMouseX = pointer.x;
+			this.lastMouseY = pointer.y;
 		});
 
 	}
