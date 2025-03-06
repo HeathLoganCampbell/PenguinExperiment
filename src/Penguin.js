3f1bbdcdd6c57f737dbf538fd50d3879505eb050
HeathLoganCampbell <Heath.campbell2@datacom.co.nz>
2025-03-06 23:27:48 +1100

    move penguin stuff to it's own class

---
+++ src/Penguin.js
@@ -0,0 +1,58 @@
+export default class Penguin extends Phaser.GameObjects.Container
+{
+    constructor(scene)
+    {
+        super(scene)
+        this.count = 0;
+        this.createPenguin();
+        this.setupMovement();
+    }
+
+    createPenguin() {
+        this.body = this.scene.add.sprite(777, 537, "penguin_1", "body/1_1");
+        this.body.tintTopLeft = 6684825;
+		this.body.tintTopRight = 6684825;
+		this.body.tintBottomLeft = 6684825;
+		this.body.tintBottomRight = 6684825;
+        this.penguin = this.scene.add.sprite(777, 537, "penguin_1", "penguin/1_1");
+        this.nametag = this.scene.add.text(777, 532, "Penguin", {
+            align: "center", 
+            color: "#000000", 
+            fontFamily: "Arial", 
+            fontSize: "24px"
+        }).setOrigin(0.5, -0.8);
+    }
+
+     setupMovement() {
+        this.scene.time.addEvent({
+			delay: 50,
+			callback: () => {
+				this.count++;
+			},
+			loop: true 
+		});
+        this.scene.input.on('pointerdown', (pointer) => this.moveTo(pointer.x, pointer.y));
+    }
+
+    moveTo(targetX, targetY) {
+        if (this.scene.matter.containsPoint(this.scene.Walls, targetX, targetY)) return;
+
+        const dx = targetX - this.penguin.x;
+        const dy = targetY - this.penguin.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        const duration = (distance / 155) * 1000;
+        const directionId = Math.min(16, Math.round((Math.atan2(dy, dx) * (180 / Math.PI) - 90 + 360) % 360 / 45) + 9);
+
+        this.penguin.setTexture("penguin_1", `penguin/${directionId}_1`);
+        this.scene.tweens.add({
+            targets: [this.penguin, this.body, this.nametag],
+            x: targetX, y: targetY,
+            duration: duration, ease: 'Linear',
+            onUpdate: () => {
+                const walkFrame = (this.count % 8) + 1;
+                this.penguin.setTexture("penguin_1", `penguin/${directionId}_${walkFrame}`);
+                this.body.setTexture("penguin_1", `body/${directionId}_${walkFrame}`);
+            }
+        });
+    }
+}
--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -5,6 +5,7 @@
 
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
+import Penguin from '../Penguin.js';
 
 export default class Level extends Phaser.Scene {
 
@@ -31,16 +32,7 @@ export default class Level extends Phaser.Scene {
 		const image_2 = this.add.image(103, 282, "cave", "door");
 		image_2.setOrigin(0, 0);
 
-		// body
-		const body = this.add.sprite(777, 537, "penguin_1", "body/1_1");
-		body.tintTopLeft = 6684825;
-		body.tintTopRight = 6684825;
-		body.tintBottomLeft = 6684825;
-		body.tintBottomRight = 6684825;
-
-		// penguin
-		const penguin = this.add.sprite(777, 537, "penguin_1", "penguin/1_1");
-
+		this.penguin = new Penguin(this);
 		// image_6
 		const image_6 = this.add.image(1369, 699, "cave", "fg_2");
 		image_6.setOrigin(0, 0);
@@ -57,35 +49,18 @@ export default class Level extends Phaser.Scene {
 		const image_4 = this.add.image(-3, 688, "cave", "fg_3");
 		image_4.setOrigin(0, 0);
 
-		// nametag
-		const nametag = this.add.text(777, 532, "", {});
-		nametag.setOrigin(0.5, -0.8);
-		nametag.text = "New text";
-		nametag.setStyle({ "align": "center", "color": "#000000", "fontFamily": "Arial", "fontSize": "24px" });
-
 		// lists
 		const displayList = [sprite_1, image_1, image_2, image_3];
-		const penguinList = [penguin, body, nametag];
 
-		this.body = body;
-		this.penguin = penguin;
-		this.nametag = nametag;
 		this.displayList = displayList;
-		this.penguinList = penguinList;
 
 		this.events.emit("scene-awake");
 	}
 
-	/** @type {Phaser.GameObjects.Sprite} */
-	body;
-	/** @type {Phaser.GameObjects.Sprite} */
+	/** @type {Penguin} */
 	penguin;
-	/** @type {Phaser.GameObjects.Text} */
-	nametag;
 	/** @type {Array<Phaser.GameObjects.Sprite|Phaser.GameObjects.Image>} */
 	displayList;
-	/** @type {Array<Phaser.GameObjects.Sprite|Phaser.GameObjects.Text>} */
-	penguinList;
 
 	/* START-USER-CODE */
 
@@ -95,20 +70,6 @@ export default class Level extends Phaser.Scene {
 		this.editorCreate();
 
 		this.count = 0; 
-		this.nametag.text = "Penguin";
-
-		this.time.addEvent({
-			delay: 100,
-			callback: () => {
-				this.count++;
-			},
-			loop: true 
-		});
-
-		this.input.on('pointermove', (pointer) => {
-			this.lastMouseX = pointer.x;
-			this.lastMouseY = pointer.y;
-		});
 
 		this.matter.world.setBounds(0, 0, 1520, 960);
 		var body = this.matter.add.fromPhysicsEditor(0, 0, this.cache.json.get('cave-physics')["Walls"])
@@ -116,47 +77,6 @@ export default class Level extends Phaser.Scene {
 		body.isStatic = true;
 		this.matter.body.setPosition(body, body.centerOffset)
 		this.walls = body;
-
-		this.input.on('pointerdown', (pointer) => {
-			const targetX = pointer.x;
-			const targetY = pointer.y;
-
-			if(this.matter.containsPoint(this.walls, targetX, targetY))
-			{
-				return;
-			}
-
-			if (this.currentTween) {
-				this.currentTween.remove();
-			}
-
-			const dx = targetX - this.penguin.x;
-			const dy = targetY - this.penguin.y;
-			const distance = Math.sqrt(dx * dx + dy * dy);
-
-			const duration = (distance / 155) * 1000;
-
-			const rawAngle = Math.floor((Math.atan2(dy, dx) * (180 / Math.PI)) - 90);
-			const angle = (rawAngle < 0) ? rawAngle + 360 : rawAngle;
-			let directionId = Math.round(angle / 45) + 9;
-			if(directionId > 16) directionId = 16;
-
-			this.penguin.setTexture("penguin_1", `penguin/${directionId}_1`);
-			this.body.setTexture("penguin_1", `body/${directionId}_1`);
-
-			this.currentTween = this.tweens.add({
-				targets: this.penguinList,
-				x: targetX,
-				y: targetY,
-				duration: duration, 
-				ease: 'Linear',
-				onUpdate: () => {
-					const walkFrame = (this.count % 8) + 1;
-					this.penguin.setTexture("penguin_1", `penguin/${directionId}_${walkFrame}`);
-					this.body.setTexture("penguin_1", `body/${directionId}_${walkFrame}`);
-				}
-			});
-		});
 	}
 
 	/* END-USER-CODE */
