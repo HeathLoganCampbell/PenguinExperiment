293d41200070ce743ff4a6146fc96ca9fb0d163f
HeathLoganCampbell <Heath.campbell2@datacom.co.nz>
2025-03-08 23:38:21 +1100

    add usernames

--- Client/src/scenes/FindFourr.js
+++ Client/src/scenes/FindFourr.js
@@ -47,9 +47,11 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 		this.add(token_blue_1);
 
 		// columnscontainer
-		const columnscontainer = scene.add.container(-168, 105);
+		const columnscontainer = scene.add.container(-182, 105);
 		columnscontainer.blendMode = Phaser.BlendModes.SKIP_CHECK;
 		columnscontainer.setInteractive(new Phaser.Geom.Rectangle(-23, -160, 340, 320), Phaser.Geom.Rectangle.Contains);
+		columnscontainer.scaleX = 1.1;
+		columnscontainer.scaleY = 1.1;
 		this.add(columnscontainer);
 
 		// column_1
@@ -89,6 +91,30 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 		column_7.setOrigin(0.5, 0.5000001);
 		columnscontainer.add(column_7);
 
+		// red_username
+		const red_username = scene.add.text(-99, 315, "", {});
+		red_username.setOrigin(0, 0.00001);
+		red_username.text = "RED USERNAME";
+		red_username.setStyle({ "fixedWidth": 260, "fontFamily": "Arial", "fontSize": "32px", "stroke": "#000", "strokeThickness": 9 });
+		this.add(red_username);
+
+		// image_1
+		const image_1 = scene.add.image(-149, 333, "four", "button/counter_1");
+		image_1.setOrigin(0.5, 0.500001);
+		this.add(image_1);
+
+		// image
+		const image = scene.add.image(-151, 403, "four", "button/counter_2");
+		image.setOrigin(0.5, 0.500001);
+		this.add(image);
+
+		// blue_username
+		const blue_username = scene.add.text(-98, 384, "", {});
+		blue_username.setOrigin(0, 0.0001);
+		blue_username.text = "BLUE USERNAME";
+		blue_username.setStyle({ "color": "#d5f1ff", "fixedWidth": 260, "fontFamily": "Arial", "fontSize": "32px", "stroke": "#336699", "strokeThickness": 9 });
+		this.add(blue_username);
+
 		// lists
 		const columns = [column_7, column_6, column_5, column_4, column_3, column_2, column_1];
 
@@ -106,6 +132,10 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 		this.column_6 = column_6;
 		this.column_7 = column_7;
 		this.columnscontainer = columnscontainer;
+		this.red_username = red_username;
+		this.image_1 = image_1;
+		this.image = image;
+		this.blue_username = blue_username;
 		this.columns = columns;
 
 		/* START-USER-CTR-CODE */
@@ -118,14 +148,6 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 		this.column_6.setInteractive();
 		this.column_7.setInteractive();
 		this.columnscontainer.setInteractive();
-
-		// this.column_1.setOrigin(0.5, 0.5);
-		// this.column_2.setOrigin(0.5, 0.5);
-		// this.column_3.setOrigin(0.5, 0.5);
-		// this.column_4.setOrigin(0.5, 0.5);
-		// this.column_5.setOrigin(0.5, 0.5);
-		// this.column_6.setOrigin(0.5, 0.5);
-		// this.column_7.setOrigin(0.5, 0.5);
 
 		this.board.setDepth(100)
 
@@ -161,6 +183,14 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 	column_7;
 	/** @type {Phaser.GameObjects.Container} */
 	columnscontainer;
+	/** @type {Phaser.GameObjects.Text} */
+	red_username;
+	/** @type {Phaser.GameObjects.Image} */
+	image_1;
+	/** @type {Phaser.GameObjects.Image} */
+	image;
+	/** @type {Phaser.GameObjects.Text} */
+	blue_username;
 	/** @type {Phaser.GameObjects.Image[]} */
 	columns;
 
@@ -202,30 +232,13 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 		this.blockedPlacing = false;
 		this.updateVisibleTokens();
 
-		let debugGraphics = this.scene.add.graphics();
-		debugGraphics.lineStyle(2, 0xff0000, 1);  // Red outline
-		const bounds = this.columnscontainer.getBounds();  // Get the bounds of the container
-		debugGraphics.strokeRect(bounds.x, bounds.y, bounds.width, bounds.height);  // Draw outline
-		debugGraphics.fillStyle(0xff0000, 0.3);  // Semi-transparent red fill
-		debugGraphics.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
-		debugGraphics.setDepth(10000)
-
-		this.columnscontainer.on('pointerout', (pointer) =>
-		{
-			console.log("hovered off columns");
-			this.token_red_1.visible = false;
-			this.token_blue_1.visible = false;
-			console.log(this.columnscontainer.getBounds())
-			console.log(this.columnscontainer.getBounds().contains(pointer.x, pointer.y))
-			if (!this.columnscontainer.getBounds().contains(pointer.x, pointer.y)) {
-				console.log("=-=-=-= Pointer left the container and its children");
-			}
-		});
-
-		this.columnscontainer.on('pointerover', (pointer) =>
-		{
-			console.log("ON CONTAINER");
-		});
+		// let debugGraphics = this.scene.add.graphics();
+		// debugGraphics.lineStyle(2, 0xff0000, 1);  // Red outline
+		// const bounds = this.columnscontainer.getBounds();  // Get the bounds of the container
+		// debugGraphics.strokeRect(bounds.x, bounds.y, bounds.width, bounds.height);  // Draw outline
+		// debugGraphics.fillStyle(0xff0000, 0.3);  // Semi-transparent red fill
+		// debugGraphics.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+		// debugGraphics.setDepth(10000)
 
 		this.columns.forEach((column, index) => {
 			var columnIndex = 6 - index;
@@ -340,12 +353,28 @@ export default class FindFourr extends Phaser.GameObjects.Container {
 			// reds turn
 			this.token_red_1.visible = true;
 			this.token_blue_1.visible = false;
+
+			console.log("ysernnammee")
+			console.log(this.red_username)
+			this.red_username.setColor('#fff')
+			this.red_username.setStroke('#000')
+
+			this.blue_username.setColor('#d5f1ff');
+			this.blue_username.setStroke('#336699');
 		}
 		else 
 		{
 			// blues turn
 			this.token_red_1.visible = false;
 			this.token_blue_1.visible = true;
+
+			console.log("ysernnammee")
+			console.log(this.blue_username)
+			this.blue_username.setColor('#fff')
+			this.blue_username.setStroke('#000')
+
+			this.red_username.setColor('#d5f1ff');
+			this.red_username.setStroke('#336699');
 		}
 	}
 
